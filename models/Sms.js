c6e61465159afabc94e817c196ca0222024fa4a2
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-05 22:02:02 +0200

    Create Sms.js

---
+++ models/Sms.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SmsSchema = new Schema({
+  deviceId: { type: String, index: true },
+  sender: String,
+  message: String,
+  timestamp: Date,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Sms', SmsSchema);
