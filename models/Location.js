38107268495f1cc136e0d71fd44149b0770eab65
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-05 22:02:59 +0200

    Create Location.js

---
+++ models/Location.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LocationSchema = new Schema({
+  deviceId: { type: String, index: true },
+  lat: Number,
+  lon: Number,
+  accuracy: Number,
+  timestamp: Date,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Location', LocationSchema);
